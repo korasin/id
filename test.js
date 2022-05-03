4599aadc7a46e6a47198dd649c0636fc1ed44c31
korasin <mchemutai232@gmail.com>
2022-05-03 21:28:28 +0300

    Add functions

--- test.js
+++ test.js
@@ -23,5 +23,78 @@ function test(){
     $("h2#header2").html("Some Text Here")
 }
 
+   // console.log(Age)
+    // console.log(Nationality)
+//     switch((Age >= 18) ){
+//         console.log(Age)
+//         case document.getElementById('Age').value:
+//             document.write('you qualify');
+//             break;
+//     switch((Age < 18) ){
+//         case document.getElementById('Age').value:
+//             document.write('you do not qualify');
+//             break;
+//     switch((Nationality == Kenyan) ){
+//         case document.getElementById('Age').value:
+//             document.write('you qualify');
+//             break;
+//     switch((Nationality != Kenyan) ){
+//         case document.getElementById('Age').value:
+//             document.write('you do not qualify');
+//             break;
+//     };
+// };
+
+// function display(){
+//     if((Age >= 18) && (Nationality=='Kenyan')){
+//         $("button#result").html("you qualify")
+//         return document.getElementById('result').value;  
+//     }else if((Nationality!='Kenyan') && (Age < 18) ){
+//         $("button#result").html("you do not qualify")
+//         return document.getElementById('result').value; 
+//     }else{
+//         $("button#result").html("you are not qualified")
+//         return document.getElementById('result').value; 
+//     }; 
+// };
+// function met(){
+//     document.write(display())
+// }
+// display('You are qualified');
+
+// function display(){
+//      console.log('tita')
+//     document.getElementById('sub').val
+//     event.preventDefault()
+// }
+// }
+
+
+// function display(){
+//     document.getElementById('Nationality').value 
+//     if(age<18){
+//         return 'you are not qualified'
+//     }else{
+//         return 'you are quaslified'
+//     }
+// }else if{
+
+// }
+//function real(){
+//     if(Nationality==Kenyan){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// let Age = document.getElementById('Age').value;
+// let Nationality = document.getElementById('Nationality').value;
+// let result = ""
+
+// console.log("Javascript loaded")
+
+
+
+
 
 
