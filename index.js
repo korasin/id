a0acf29e826866a7e2e496ee5171f20276559ac5
korasin <mchemutai232@gmail.com>
2022-05-03 21:17:34 +0300

    Add changes

--- index.js
+++ index.js
@@ -15,23 +15,71 @@
 //         return false
 //     }
 // }
+// let Age = document.getElementById('Age').value;
+// let Nationality = document.getElementById('Nationality').value;
+// let result = ""
 
-let Age = document.getElementById('Age').value
-let Nationality = document.getElementById('Nationality').value
-// let result = document.getElementById('result').value = display()
+// console.log("Javascript loaded")
+
+
 
 function display(){
-    if((Age >= 18) && (Nationality=='Kenyan')){
-        $("button#result").html("you qualify")
-        return document.getElementById('result').value  
-    }else if((Nationality!='Kenyan') && (Age < 18) ){
-        $("button#result").html("you do not qualify")
-        return document.getElementById('result').value 
-    }else{
+    let Age = document.getElementById('Age').value; 
+    let Nationality = document.getElementById('Nationality').value; 
+
+    if ((Age >= 18)  && (Nationality == "Kenyan")) {
+    // console.log('You are qualified')
+    $("button#result").html("you qualify")
+        // document.getElementById("result")='you are qualified'
+    }else if((Age < 18) && (Nationality != "Kenyan")){
+        // console.log('You are not qualified')
         $("button#result").html("you are not qualified")
-        return document.getElementById('result').value 
-    }; 
-};
+        // document.write('You are not qualified')
+    }else{
+        // console.log('you do not meet all requirements')
+        $("button#result").html("you do not meet all requirements")
+        // document.write('you do not meet all requirements') 
+    }
+}
+
+
+
+
+
+     // console.log(Age)
+    // console.log(Nationality)
+//     switch((Age >= 18) ){
+//         console.log(Age)
+//         case document.getElementById('Age').value:
+//             document.write('you qualify');
+//             break;
+//     switch((Age < 18) ){
+//         case document.getElementById('Age').value:
+//             document.write('you do not qualify');
+//             break;
+//     switch((Nationality == Kenyan) ){
+//         case document.getElementById('Age').value:
+//             document.write('you qualify');
+//             break;
+//     switch((Nationality != Kenyan) ){
+//         case document.getElementById('Age').value:
+//             document.write('you do not qualify');
+//             break;
+//     };
+// };
+
+// function display(){
+//     if((Age >= 18) && (Nationality=='Kenyan')){
+//         $("button#result").html("you qualify")
+//         return document.getElementById('result').value;  
+//     }else if((Nationality!='Kenyan') && (Age < 18) ){
+//         $("button#result").html("you do not qualify")
+//         return document.getElementById('result').value; 
+//     }else{
+//         $("button#result").html("you are not qualified")
+//         return document.getElementById('result').value; 
+//     }; 
+// };
 // function met(){
 //     document.write(display())
 // }
@@ -41,4 +89,5 @@ function display(){
 //      console.log('tita')
 //     document.getElementById('sub').val
 //     event.preventDefault()
+// }
 // }
