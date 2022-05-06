e3ad724bce162588fe83a5059fb1d903098d8d0a
korasin <mchemutai232@gmail.com>
2022-05-06 11:36:56 +0300

    clear button

---
+++ mr.js
@@ -0,0 +1,14 @@
+// var name = ['kip', 'vinn','kira']
+
+// function sum(){
+//     if(name = name[0]){
+//         console.log('i am ready')
+//     }else if (name = name[1]){
+//         console.log('i hve taken my meal')
+//     }else if(name = name[02]){
+//         console.log('i am done')
+//     }else{
+//         console.log(" i haven't taken")
+//     }
+    
+// }
